43bd7243ba87b9a88b176973fc9c35c8e4f20c5a
pocoyo-da-krebrada <164937314+pocoyo-da-krebrada@users.noreply.github.com>
2024-06-04 14:42:37 -0300

    :sparkles: build: reformulação do users.service
    Retirada de código gambiarra e estabelecimento de código revisado.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,115 @@
-import { Injectable } from '@nestjs/common';
-import { CrudOptions, RejectOptions } from '@cjr-unb/super-crud';
-import { Prisma } from '@prisma/client';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUsersDto } from './dto/create-users.dto';
+import { UpdateUsersDto } from './dto/update-users.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-
-type UserModel = Prisma.UsersDelegate<RejectOptions>;
-const {defaultOptions, getCrud} = new CrudOptions<UserModel>().setOptions({});
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService extends getCrud<
-  Prisma.UsersGetPayload<typeof defaultOptions>
->() {
-  constructor(protected readonly prisma: PrismaService) {
-    super(prisma.users, defaultOptions);
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createUsersDto: CreateUsersDto) {
+    const existingUsers = await this.prismaService.users.findUnique({
+      where: { email: createUsersDto.email },
+    });
+
+    if (existingUsers) {
+      throw new ConflictException('Email ja cadastrado');
+    }
+
+    const hashedpassword = await bcrypt.hash(createUsersDto.password, 10);
+
+    return await this.prismaService.users.create({
+      data: { ...createUsersDto, password: hashedpassword },
+    });
+  }
+  async findAll() {
+    return await this.prismaService.users.findMany({
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        coments: true,
+        createdAt: true,
+        updadteAt: true,
+      },
+    });
+  }
+  async findOne(id: number) {
+    const isValidId = await this.prismaService.users.findUnique({
+      where: { id },
+    });
+    if (!isValidId) {
+      throw new NotFoundException('user nao encontrado');
+    }
+    return await this.prismaService.users.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        coments: true,
+        createdAt: true,
+        updadteAt: true,
+      },
+    });
+  }
+
+  async findByEmail(email: string) {
+    const users = await this.prismaService.users.findUnique({
+      where: { email },
+    });
+    if (!users) {
+      return null;
+    }
+    return users;
+  }
+
+  async update(id: number, updateUsersDto: UpdateUsersDto) {
+    const isValidId = await this.prismaService.users.findUnique({
+      where: { id },
+    });
+    if (!isValidId) {
+      throw new NotFoundException('usuario nao encontrado');
+    }
+
+    const hashedpassword = await bcrypt.hash(updateUsersDto.password, 10);
+
+    return await this.prismaService.users.update({
+      where: { id },
+      data: { ...updateUsersDto, password: hashedpassword },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        coments: true,
+        createdAt: true,
+        updadteAt: true,
+      },
+    });
+  }
+  async remove(id: number) {
+    const isValidId = await this.prismaService.users.findUnique({
+      where: { id },
+    });
+    if (!isValidId) {
+      throw new NotFoundException('usuario nao encontrado');
+    }
+
+    return await this.prismaService.users.delete({
+      where: { id },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        coments: true,
+        createdAt: true,
+        updadteAt: true,
+      },
+    });
   }
 }
