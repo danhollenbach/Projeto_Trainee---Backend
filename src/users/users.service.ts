b22a5fa59819027f88893ae54360a2b0f3541432
pocoyo-da-krebrada <164937314+pocoyo-da-krebrada@users.noreply.github.com>
2024-06-03 22:11:47 -0300

    :rewind: fix: correção de uma cagada
    correção de erro feito durante o teste de rotas

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { CrudOptions, RejectOptions } from '@cjr-unb/super-crud';
 import { Prisma } from '@prisma/client';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-type UsersModel = Prisma.UsersDelegate<RejectOptions>;
-const { defaultOptions, getCrud } = new CrudOptions<UsersModel>().setOptions(
-  {},
-);
+type UserModel = Prisma.UsersDelegate<RejectOptions>;
+const {defaultOptions, getCrud} = new CrudOptions<UserModel>().setOptions({});
 
 @Injectable()
 export class UsersService extends getCrud<
